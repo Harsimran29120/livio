aef4d0d14f02588d82608dd871132c890251a0a8
Harsimran29120 <72358179+Harsimran29120@users.noreply.github.com>
2023-10-18 14:05:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+let width = screen.width;
+console.log(width)
+  var imgList = document.getElementById('imgList');
+  var scrollRight = document.getElementById('scroll-right');
+  var scrollLeft = document.getElementById('scroll-left');
+  if (width < 800) {
+// When a user clicks on the right arrow, the ul will scroll 750px to the right
+      scrollRight.addEventListener('click', (event) => {
+        imgList.scrollBy(240, 0);
+    });}
+  else {
+    scrollRight.addEventListener('click', (event) => {
+      imgList.scrollBy(870, 0);
+  });
+  }
+  if (width < 800) {
+// When a user clicks on the left arrow, the ul will scroll 750px to the left
+    scrollLeft.addEventListener('click', (event) => {
+      imgList.scrollBy(-240, 0);
+    });}
+  else {
+    scrollLeft.addEventListener('click', (event) => {
+      imgList.scrollBy(-870, 0);
+  });
+  }
+
+  
